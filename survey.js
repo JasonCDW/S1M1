6c8dd9d1761d2e17bf9980654ac46a3bd950e9bd
JasonCDW <dw.jay.12@gmail.com>
2018-08-06 20:43:11 -0400

    Added requirements for milestone 2

--- survey.js
+++ survey.js
@@ -1,9 +1,16 @@
-let myName = prompt ("What's your name?");
-while (!myName) {
-    alert ("Please enter a valid name.");
-    myName = prompt ("What's your name?");
+let myFName = prompt ("What is your first name?");
+while (!myFName) {
+    alert ("Please enter a valid first name.");
+    myFName = prompt ("What is your first name?");
 }
-console.log(myName);
+console.log(myFName);
+
+let myLName = prompt ("What is your last name?");
+while (!myLName) {
+    alert ("Please enter a valid last name.");
+    myLName = prompt ("What is your last name?");
+}
+console.log(myLName);
 
 let myAge = prompt ("How old are you?");
 while (!myAge) {
@@ -19,23 +26,34 @@ while (!favColor) {
 }
 console.log(favColor);
 
+let favArtist = prompt ("Who is your favorite musician?");
+while (!favArtist) {
+    alert ("Please enter a valid name or stage name.");
+    favArtist = prompt ("Who is your favorite musician?");
+}
+console.log(favArtist);
+
 let employment = confirm ("Are you currently employed?");
 console.log(employment);
 if (employment === true) {
-    alert ("Your name is " + myName + ", you are " + myAge + " years old, your favorite color is " + favColor + " and you are currently employed.")
-    console.log ("Your name is " + myName + ", you are " + myAge + " years old, your favorite color is " + favColor + " and you are currently employed.")
+    alert ("Your name is " + myFName + " " + myLName + ", you are " + myAge + " years old, your favorite color is " + favColor + ", your favorite musician is " + favArtist + " and you are currently employed.")
+    console.log ("Your name is " + myFName + " " + myLName + ", you are " + myAge + " years old, your favorite color is " + favColor + ", your favorite musician is " + favArtist + " and you are currently employed.")
 }
 else {
-    alert ("Your name is " + myName + ", you are " + myAge + " years old, your favorite color is " + favColor + " and you are currently unemployed.")
-    console.log ("Your name is " + myName + ", you are " + myAge + " years old, your favorite color is " + favColor + " and you are currently unemployed.")
+    alert ("Your name is " + myFName + " " + myLName + ", you are " + myAge + " years old, your favorite color is " + favColor + ", your favorite musician is " + favArtist + " and you are currently unemployed.")
+    console.log ("Your name is " + myFName + " " + myLName + ", you are " + myAge + " years old, your favorite color is " + favColor + ", your favorite musician is " + favArtist + " and you are currently unemployed.")
 }
 
 alert ("Thank you for completing this survey.");
 console.log ("Thank you for completing this survey.");
 
-let hName = document.getElementById('name');
-hName.innerHTML = myName;
-console.log(hName);
+let hFName = document.getElementById('firstName');
+hFName.innerHTML = myFName;
+console.log(hFName);
+
+let hLName = document.getElementById('lastName');
+hLName.innerHTML = myLName;
+console.log(hLName);
 
 let hAge = document.getElementById('age');
 hAge.innerHTML = myAge;
@@ -44,6 +62,10 @@ console.log(hAge);
 let hColor = document.getElementById('color');
 hColor.innerHTML = favColor;
 console.log(hColor);
+
+let hArtist = document.getElementById('artist');
+hArtist.innerHTML = favArtist;
+console.log(hArtist);
 
 let hEmployment = document.getElementById('employment');
 hEmployment.innerHTML = employment;
